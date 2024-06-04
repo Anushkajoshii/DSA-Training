650d315c619b14e212e209d2665f71438db2e17a
Anushkajoshii <anushkajoshi020503@gmail.com>
2024-06-04 19:48:12 +0530

    this is my first commit

---
+++ stack/Queue using stack.cpp
@@ -0,0 +1,27 @@
+//gfg
+
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) {
+        input.push(x);
+    }
+
+    int dequeue() {
+        while(input.empty()) return -1;
+        
+        while(!input.empty()){
+            output.push(input.top());
+            input.pop();
+        }
+        int ans= output.top();
+        output.pop();
+        
+        while(!output.empty()){
+            input.push(output.top());
+            output.pop();
+        }
+        return ans;
+    }
+};
